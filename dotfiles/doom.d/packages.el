8ad000e8ef7f75c8ef2399362fa3f28d10619419
Vieko Franetovic <vieko.franetovic@gmail.com>
2019-02-28 15:18:59 -0700

    added initial emacs configuration
    Signed-off-by: Vieko Franetovic <vieko.franetovic@gmail.com>

---
+++ dotfiles/doom.d/packages.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+(package! gruvbox-theme)
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
+;; (package! builtin-package :disable t)
